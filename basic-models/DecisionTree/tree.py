17392d32da79ba3a92f5232790d7ebc799599eab
Sai C Akella <sai.c.akella@gmail.com>
2024-05-31 09:45:28 +1000

    implemented Decision Tree for regression from scratch

--- basic-models/DecisionTree/tree.py
+++ basic-models/DecisionTree/tree.py
@@ -181,6 +181,168 @@ class DTreeClassifier:
         y_pred=[self.__x_predict(tree, x) for x in x_test]
         return np.array(y_pred)
 
+
+
+
+class DTreeRegressor:
+
+    def __init__(self, max_depth=None):
+
+        self.max_depth=max_depth 
+        self.x_train=None 
+        self.y_train=None 
+        self.estimator1=dict() # detailed result
+        self.estimator2=dict() # summarized result
+        '''
+            tree 
+            ----
+            {   'is_leaf':
+                'fid':
+                'split_point':
+                'left_child': {'is_leaf': , 'ids':[...]} / {'is_leaf': ,'fid': , 'split_point': , ...}
+                'right_child': {'is_leaf': , 'ids':[...]} / {'is_leaf': ,'fid': , 'split_point': , ...}
+            }
+        '''
+
+
+    '''Split a node into left and right children'''
+    '''Aim: Find the best split point with lowest MSE'''
+    def __node_split(self, data_ids):
+        '''
+            data_ids: contains the id's of the data points present in the node
+        '''
+        n=data_ids.shape[0]
+
+
+        # Split the node into all candidates for all features and
+        # Find the best feature and the best split point with the
+        # lowest MSE
+        min_mse = 999999
+        for fid in range(self.x_train.shape[1]):
+
+            # feature data to be split
+            feature_data = self.x_train[data_ids, fid].copy()
+
+            # remove duplicates of feature_data and sort in ascending order
+            unique_feature_data = np.unique(feature_data)
+
+            # list all the candidates, ehich are the midpoints of adjacent data
+            candidate_split_points = [np.mean(unique_feature_data[[i,i+1]]) \
+                                      for i in range(len(unique_feature_data)-1)]
+            
+                                      
+            '''
+               len(candidate_split_points) > 1:
+                   Calculate the mse for all candidates, and find the candidate with the lowest mse.
+               len(candidate_split_points) < 1:
+                   Skip the for-loop. feature_data either has only one data point or all have the same value. 
+                   No need to split.
+            '''
+
+            for c in candidate_split_points:
+                #split feature_data into left and right nodes
+                left=data_ids[np.where(feature_data<=c)[0]]
+                right=data_ids[np.where(feature_data>c)[0]]
+
+                # Calculate MSE after splitting
+                l_mse=self.y_train[left].var()
+                r_mse=self.y_train[right].var()
+
+                # Calculate IG
+                overall_mse=l_mse * (left.shape[0]/n) + r_mse * (right.shape[0]/n)
+
+                # find where the MSE is smallest
+                if overall_mse < min_mse:
+                    min_mse=overall_mse
+                    b_fid=fid # best feature id
+                    b_point=c # best split point
+                    l_node=left # data ids in left node
+                    r_node=right # data ids in right node
+                   
+            
+        if min_mse < 999999: # split
+            return dict([('is_leaf',False),('fid',b_fid),('split_point',b_point),\
+                         ('left_child',{'is_leaf':True,'ids':l_node}),('right_child',{'is_leaf':True,'ids':r_node})])
+        else:
+            return None # No split
+
+
+
+    # Create a binary tree using recursion
+    def __recursive_split(self, node, curr_depth):
+        left=node['left_child']['ids']
+        right=node['right_child']['ids']
+
+        # exit recursion
+        if curr_depth>=self.max_depth:
+            return
+
+        # recursion
+        s=self.__node_split(left)
+        if isinstance(s, dict): # split to the left, done.
+            node['left_child']=s
+            self.__recursive_split(node['left_child'], curr_depth+1)
+            
+
+        s=self.__node_split(right)
+        if isinstance(s, dict): # split to the right, done.
+            node['right_child']=s
+            self.__recursive_split(node['right_child'], curr_depth+1)
+
+    
+   
+    # Change the data ids in the leaf node to average value
+    def __update_leaf(self, node):
+        if node['is_leaf']==False:
+            for key, value in node.items():
+                if key == 'left_child' or key == 'right_child':
+                    rtn=self.__update_leaf(value)
+                    if rtn[0] == 1: # leaf node
+                        node[key]=rtn[1]
+            return 0,0 # first 0 means this is not a leaf node
+        else:
+            return 1, np.mean(self.y_train[node['ids']]) # first 1 means this is a leaf node
+            
+   
+    # Create a tree using training data, and return the result of the tree
+    def fit(self, x_train, y_train):
+        self.x_train=x_train
+        self.y_train=y_train
+
+        # Initially, the root node holds all the data indices.
+        root=self.__node_split(np.arange(self.x_train.shape[0]))
+        if isinstance(root, dict):
+            self.__recursive_split(root, curr_depth=1)
+
+
+        self.estimator1=root # estimator1's leaf nodes contains data indices
+
+        self.estimator2=copy.deepcopy(self.estimator1)
+        self.__update_leaf(self.estimator2)
+        
+
+
+
+    # Estimate the target class of a test data
+    def __x_predict(self,tree,x):
+        if x[tree['fid']] <= tree['split_point']:
+            if isinstance(tree['left_child'], dict): # recursion if not leaf
+                return self.__x_predict(tree['left_child'], x) # recursion
+            else:
+                return tree['left_child']
+        else:
+            if isinstance(tree['right_child'], dict): # recursion if not leaf
+                return self.__x_predict(tree['right_child'], x) # recursion
+            else:
+                return tree['right_child']
+
+
+    # Estimate the target class of x_test
+    def predict(self, x_test):
+        tree=self.estimator2 # predictor
+        y_pred=[self.__x_predict(tree, x) for x in x_test]
+        return np.array(y_pred)
+
     
                 
         
