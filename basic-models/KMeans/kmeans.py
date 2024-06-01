9b5a51d9eeeaf86a8725ffe33e7cf38dc17c182b
Sai C Akella <sai.c.akella@gmail.com>
2024-06-01 16:17:16 +1000

    implemented k-means++ algorithm from scratch

--- basic-models/KMeans/kmeans.py
+++ basic-models/KMeans/kmeans.py
@@ -4,7 +4,7 @@ import time
 
 class KMeans:
 
-    def __init__(self,n_clusters=3,n_init=10,max_iter=10):
+    def __init__(self,n_clusters=3,n_init=10,max_iter=10,init='random'):
         '''
             n_clusters: The number of clusters
             
@@ -12,11 +12,19 @@ class KMeans:
                     prevent local minimum problem.
 
             max_iter: Maximum number of iterations of the k-means algorithm for a single run.
+
+            init: Method for centroids initialization
+                  {‘k-means++’, ‘random’}
             
         '''
-
+        self.init=init
         self.n_clusters=n_clusters
-        self.n_init=n_init
+        
+        if self.init=='k-means++'
+            self.n_init=1
+        else:
+            self.n_init=n_init
+            
         self.max_iter=max_iter
         
 
@@ -28,9 +36,9 @@ class KMeans:
         c = centroids[np.newaxis,:] 
         
                                                                  
-        training = data[:,np.newaxis] 
+        d = data[:,np.newaxis] 
         
-        d_matrix = np.sqrt(np.sum(np.square(c - training),axis=2)) # shape= no. of data points X number of centroids
+        d_matrix = np.sqrt(np.sum(np.square(c - d),axis=2)) # shape= no. of data points X number of centroids
 
         '''
             c_1 c_2 ... c_n
@@ -61,15 +69,52 @@ class KMeans:
         plt.scatter(centroids[:,0],centroids[:,1],s=250,c='black')
         plt.scatter(centroids[:,0],centroids[:,1],s=80,c='yellow')
         plt.show()
+
+
+    # generate initial centroids using k-means++ algorithm
+    def __kmeanspp(self,data):
+
+        xp=data.copy() # centroid candidates
+        centroids=[]
+        density=np.ones(xp.shape[0])/N # uniform distribution is used as initial probability distribution
+
+        for c in range(self.n_clusters):
+
+            # (1) choose an initial centroid c(1) uniformly at random from X
+            # (2) choose the next centroi c(i), selecting c(i) = x' E X with the probability density function
+
+            idx=np.random.choice(np.arange(xp.shape[0]),p=density)
+            centroids.append(xp[idx])
+            xp=np.delete(xp,idx,axis=0)
+
+            # Create a distance matrix b/w data points xp and the centroids.
+            dist=self.__calculate_distance(np.array(centroids),xp)
+
+            # find the centroid closest to each data point
+            assign = np.argmin(dist,axis=1)
+
+            # calculate D(x)
+            # let D(x) denote the shortest distance from a data point x to the closest centroid we have already chosen
+            Dx=np.sum(np.square(xp-np.array(centroids)[assign]),axis=1)
+
+            # create a probability density function to select the next centroid
+            density=Dx/np.sum(Dx)
+
+
+        return np.array(centroids)
+        
+            
+        
        
 
 
     def fit(self,data):
-
+        
         self.f_error=[999999] # final error history
         self.f_centroids=None # final centroids
 
-        #cluster_colors= self.__generate_colors()
+        if self.n_init==1:
+            cluster_colors= self.__generate_colors()
         
         N=data.shape[0] # the number of data points
 
@@ -77,10 +122,14 @@ class KMeans:
                                      # positions of centroids
 
             # Initialize the centroids.
-            # Randomly select n_clusters data points and use them as initial centroids.
-            idx=np.random.choice(np.arange(N),self.n_clusters)
-            centroids=data[idx]
-            error=[]
+            if self.init=='random':
+                # Randomly select n_clusters data points and use them as initial centroids.
+                idx=np.random.choice(np.arange(N),self.n_clusters)
+                centroids=data[idx]
+            else:
+                centroids=self.__kmeanspp(data)
+                
+            error=[] # error histor for current run of algorithm
 
             for _ in range(self.max_iter):
 
@@ -109,15 +158,17 @@ class KMeans:
                 error.append(err) # tracking error history as centroids shift
 
                 # To observe centroids moving set n_init=1
-                #self.__plot_cluster(data,assign,centroids,cluster_colors)
-                #time.sleep(1)
+                if self.n_init==1:
+                    self.__plot_cluster(data,assign,centroids,cluster_colors)
+                    time.sleep(1)
 
                 # update centroids
                 centroids=np.array(new_cent)
 
 
-            # Among the n_init number of iterations, the one with thr smallest error
+            # Among the n_init number of iterations, the one with the smallest error
             # is selected as the final result
+            # in case init is set to 'k-means++' n_init will be 1.
             if error[-1]<self.f_error[-1]:
                 self.f_error=np.copy(error)
                 self.f_centroids=np.copy(centroids)
