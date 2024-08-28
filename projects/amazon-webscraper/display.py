936e416867919721043d3604fb48053801d0c172
Sai C Akella <sai.c.akella@gmail.com>
2024-08-28 12:18:22 +1000

    restructured repo

--- projects/amazon-webscraper/display.py
+++
@@ -1,9 +0,0 @@
-def retrieveData(search_query,cursor):
-    query = f'SELECT * FROM {search_query}.products'
-    cursor.execute(query)
-    all_products = cursor.fetchall()
-
-    return all_products
-
-
-    
